88d05173224edd9d7f2ab2c8dcb8420065a213b5
Jordan Osborn <jordanosborn0@gmail.com>
2019-01-09 14:38:44 +0000

    fixed folder structure

---
+++ raytracer/src/args.rs
@@ -0,0 +1,11 @@
+pub fn parse_args() -> (i32, i32) {
+    match std::env::args().collect::<Vec<String>>().as_slice() {
+        [_, a, b] => {
+            match (a.parse::<i32>(), b.parse::<i32>()) {
+                (Ok(a), Ok(b)) => {(a, b)}
+                _ => panic!("Incorrect command line args given")
+            }
+        }
+        _ => panic!("Incorrect command line args given")
+    }
+}
--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -1,68 +1,10 @@
-fn parse_args() -> (i32, i32) {
-    match std::env::args().collect::<Vec<String>>().as_slice() {
-        [_, a, b] => {
-            match (a.parse::<i32>(), b.parse::<i32>()) {
-                (Ok(a), Ok(b)) => {(a, b)}
-                _ => panic!("Incorrect command line args given")
-            }
-        }
-        _ => panic!("Incorrect command line args given")
-    }
-}
-
-struct Vec3 {
-    pub data: [f64;3]
-}
-
-impl Vec3 {
-    pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
-        Vec3 {data: [x, y, z]}
-    }
-    #[inline] 
-    pub fn x(&self) -> f64 {self.data[0]}
-    #[inline] 
-    pub fn y(&self) -> f64 {self.data[0]}
-    #[inline] 
-    pub fn z(&self) -> f64{self.data[0]}
-    #[inline] 
-    pub fn r(&self) -> f64 {self.data[0]}
-    #[inline] 
-    pub fn g(&self) -> f64 {self.data[0]}
-    #[inline] 
-    pub fn b(&self) -> f64 {self.data[0]}
-
-    #[inline]
-    pub fn length(&self) -> f64 {
-        (self.data[0] * self.data[0] + self.data[1] * self.data[1] + self.data[2] * self.data[2]).sqrt()
-    }
-    #[inline]
-    pub fn squared_length(&self) -> f64 {
-        self.data[0] * self.data[0] + self.data[1] * self.data[1] + self.data[2] * self.data[2]
-    }
-    #[inline]
-    pub fn make_unit_vector(&self) -> Vec3 {
-        let length = (self.data[0] * self.data[0] + self.data[1] * self.data[1] + self.data[2] * self.data[2]).sqrt();
-        Vec3{
-            data: [self.data[0] / length, self.data[1] / length, self.data[2] / length]
-        }
-    }
-}
-
-
-impl std::ops::Add for Vec3 {
-    type Output = Vec3;
-    #[inline]
-    fn add(self, other: Vec3) -> Vec3 {
-        Vec3{data: [
-            self.data[0] + other.data[0],
-            self.data[1] + other.data[1],
-            self.data[2] + other.data[2]
-        ]}
-    }
-}
+mod vector;
+mod args;
+use self::vector::Vec3;
 
 fn main() {
-    let (width, height) = parse_args();
+    let x = Vec3::new(1.0, 2.0, 3.0);
+    let (width, height) = args::parse_args();
     print!("P3\n{} {}\n255\n", width, height);
 
     for j in (0..(height)).rev() {
