71d496387a711415bc69561ac896f9c22105b067
Jordan Osborn <jordanosborn0@gmail.com>
2019-01-10 09:50:35 +0000

    finished vector functionality

--- raytracer/src/vector/mod.rs
+++ raytracer/src/vector/mod.rs
@@ -56,6 +56,30 @@ impl Vec3 {
     }
 }
 
+impl std::ops::Index<usize> for Vec3 {
+    type Output = f64;
+    fn index(&self, idx: usize) -> &f64 {
+        if idx < 3 {
+            &self.data[idx]
+        } else {
+            panic!("Index invalid")
+        }
+
+    }
+}
+
+impl std::ops::IndexMut<usize> for Vec3 {
+    fn index_mut(&mut self, idx: usize) -> &mut f64 {
+        if idx < 3 {
+            &mut self.data[idx]
+        } else {
+            panic!("Index invalid")
+        }
+
+    }
+}
+
+
 impl std::ops::Add for Vec3 {
     type Output = Vec3;
     #[inline]
@@ -69,6 +93,124 @@ impl std::ops::Add for Vec3 {
         }
     }
 }
+
+impl std::ops::Sub for Vec3 {
+    type Output = Vec3;
+    #[inline]
+    fn sub(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            data: [
+                self.data[0] + other.data[0],
+                self.data[1] + other.data[1],
+                self.data[2] + other.data[2],
+            ],
+        }
+    }
+}
+
+impl std::ops::Mul<Vec3> for f64 {
+    type Output = Vec3;
+    #[inline]
+    fn mul(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            data: [
+                self * other.data[0],
+                self * other.data[1],
+                self * other.data[2]
+            ],
+        }
+    }
+}
+
+impl std::ops::Mul<f64> for Vec3 {
+    type Output = Vec3;
+    #[inline]
+    fn mul(self, other: f64) -> Vec3 {
+        Vec3 {
+            data: [
+                self.data[0] * other,
+                self.data[1] * other,
+                self.data[2] * other
+            ],
+        }
+    }
+}
+
+impl std::ops::Div<f64> for Vec3 {
+    type Output = Vec3;
+    #[inline]
+    fn div(self, other: f64) -> Vec3 {
+        if other != 0.0 {
+            Vec3 {
+                data: [
+                    self.data[0] / other,
+                    self.data[1] / other,
+                    self.data[2] / other
+                ],
+            }
+        } else {
+            panic!("Can't divide by zero")
+        }
+    }
+}
+
+impl std::ops::AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Vec3) {
+        self.data[0] = self.data[0] + other.data[0];
+        self.data[1] = self.data[1] + other.data[1];
+        self.data[2] = self.data[2] + other.data[2];
+    }
+}
+
+impl std::ops::SubAssign for Vec3 {
+    fn sub_assign(&mut self, other: Vec3) {
+        self.data[0] = self.data[0] - other.data[0];
+        self.data[1] = self.data[1] - other.data[1];
+        self.data[2] = self.data[2] - other.data[2];
+    }
+}
+
+impl std::ops::MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, other: f64) {
+        self.data[0] = self.data[0] * other;
+        self.data[1] = self.data[1] * other;
+        self.data[2] = self.data[2] * other;
+    }
+}
+
+impl std::ops::MulAssign<Vec3> for Vec3 {
+    fn mul_assign(&mut self, other: Vec3) {
+        self.data[0] = self.data[0] * other.data[0];
+        self.data[1] = self.data[1] * other.data[0];
+        self.data[2] = self.data[2] * other.data[0];
+    }
+}
+
+impl std::ops::DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, other: f64) {
+        if other != 0.0 {
+            self.data[0] = self.data[0] / other;
+            self.data[1] = self.data[1] / other;
+            self.data[2] = self.data[2] / other;
+        } else {
+            panic!("Can't divide by zero")
+        }
+    }
+}
+
+impl std::ops::DivAssign<Vec3> for Vec3 {
+    fn div_assign(&mut self, other: Vec3) {
+        if other.data[0] != 0.0 && other.data[1] != 0.0 && other.data[2] != 0.0 {
+            self.data[0] = self.data[0] / other.data[0];
+            self.data[1] = self.data[1] / other.data[0];
+            self.data[2] = self.data[2] / other.data[0];
+        } else {
+            panic!("Can't divide by zero")
+        }
+        
+    }
+}
+
 
 
 #[cfg(test)]
