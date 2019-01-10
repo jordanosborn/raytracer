8ce3bdde54c8287a285622fd39f1feab52a8f40c
Jordan Osborn <jordanosborn0@gmail.com>
2019-01-10 11:07:57 +0000

    working sphere solution

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -4,17 +4,26 @@ mod ray;
 use self::vector::Vec3;
 use self::ray::Ray;
 
-fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> bool {
+
+fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> (bool, f64) {
     let oc = r.origin() - *center;
     let a = Vec3::dot(&r.direction(), &r.direction());
     let b = 2.0 * Vec3::dot(&oc, &r.direction());
     let c = Vec3::dot(&oc, &oc) - radius * radius;
-    b * b - 4.0 * a * c > 0.0
+    let discriminant = b * b - 4.0 * a * c;
+    if discriminant < 0.0 {
+        (false, -1.0)
+    } else {
+        // closest solution only
+        (true, (-b - discriminant.sqrt()) / (2.0 * a))
+    }
 }
 
 fn color(ray: &Ray) -> Vec3 {
-    if hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, &ray) {
-        Vec3::new(1.0, 0.0, 0.0)
+    let (hit, t) = hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, &ray);
+    if hit {
+        let n = Vec3::unit_vector(&(ray.point_at_parameter(t) - Vec3::new(0.0, 0.0, -1.0)));
+        0.5 * Vec3::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0)
     } else {
         let unit_direction = Vec3::unit_vector(&ray.direction());
         let t = 0.5 * (unit_direction.y() + 1.0);
