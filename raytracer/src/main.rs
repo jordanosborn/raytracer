41f15a94bd9ef593a1a43d8cd056e0a05f2a1d3f
Jordan Osborn <jordanosborn0@gmail.com>
2019-01-10 13:50:45 +0000

    removed old code

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -27,32 +27,6 @@ fn color(ray: &Ray, world: &HitableList) -> Vec3 {
     }
 }
 
-// fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> (bool, f64) {
-//     let oc = r.origin() - *center;
-//     let a = Vec3::dot(&r.direction(), &r.direction());
-//     let b = 2.0 * Vec3::dot(&oc, &r.direction());
-//     let c = Vec3::dot(&oc, &oc) - radius * radius;
-//     let discriminant = b * b - 4.0 * a * c;
-//     if discriminant < 0.0 {
-//         (false, -1.0)
-//     } else {
-//         (true, (-b - discriminant.sqrt()) / (2.0 * a))
-//     }
-// }
-
-// fn color(r: &Ray) -> Vec3 {
-//     let (hit, t) = hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, r);
-//     if hit {
-//         let n = Vec3::unit_vector(&(r.point_at_parameter(t) - Vec3::new(0.0, 0.0, -1.0)));
-//         0.5 * Vec3::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0)
-//     } else {
-//         let unit_direction = Vec3::unit_vector(&r.direction());
-//         let t = 0.5 * (unit_direction.y() + 1.0);
-//         (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
-//     }
-// }
-
-
 fn main() {
     let (nx_i, ny_i, output) = args::parse_args();
     let (nx, ny) = (nx_i as f64, ny_i as f64);
@@ -69,8 +43,8 @@ fn main() {
     let origin = Vec3::new(0.0, 0.0, 0.0);
 
     let world = HitableList::new(vec![
-        HITABLE::SPHERE(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5)), 
-        HITABLE::SPHERE(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0))
+        HITABLE::SPHERE(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5)),
+        HITABLE::SPHERE(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0)),
     ]);
 
     if let Err(e) = output.write(format!("P3\n{} {}\n255\n", nx_i, ny_i).as_bytes()) {
