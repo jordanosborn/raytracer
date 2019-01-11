c6b8e2e378bc350def544a65f5073888bdaba332
Jordan Osborn <jordanosborn0@gmail.com>
2019-01-11 22:23:22 +0000

    Added pre commit script

---
+++ raytracer/commit.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python3
+
+import subprocess as sp
+import sys
+
+def main():
+    print("Formatting code!")
+    fmt = sp.check_output(["cargo", "fmt"])
+    try:
+        print("Linting code!")
+        lint = sp.check_output(["cargo", "clippy", "--all-features", "--", "-D", "warnings"])
+        print("Testing code!")
+        test = sp.check_output(["cargo", "test"])
+    except sp.CalledProcessError:
+        print("Failed!")
+    else:
+        sp.call(["git", "add", "-u"])
+        sp.call(["git", "commit", "-m", sys.argv[1]])
+    
+
+if __name__ == "__main__":
+    main()
