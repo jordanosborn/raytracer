c1a7d9e0ea37bc6ae8dd6c9eff62b120910189b6
Jordan Osborn <jordanosborn0@gmail.com>
2019-01-12 14:33:37 +0000

    fixed helper script

--- raytracer/raytracer.py
+++ raytracer/raytracer.py
@@ -5,10 +5,10 @@ import sys, os
 from typing import Dict
 from distutils.dir_util import copy_tree
 
-def main(cwd: str, cargo: Dict[str, str]):
+def commit(cwd: str, cargo: Dict[str, str]):
     gitworkflow = [
         ["git", "add", "-u"],
-        ["git", "commit", "-m", sys.argv[1]],
+        ["git", "commit", "-m", sys.argv[2]],
         ["git", "pull", "--rebase"],
         ["git", "push"]
     ]
@@ -19,7 +19,7 @@ def main(cwd: str, cargo: Dict[str, str]):
     fmt = sp.check_output(["cargo", "fmt"])
     print("Generating docs!")
     docs = sp.check_output(["cargo", "doc", "--no-deps"])
-    copy_tree(doc_dir, doc_dest_dir)
+    copy_tree(doc_dir, doc_dest_dir, update=1)
     try:
         print("Linting code!")
         lint = sp.check_output(["cargo", "clippy", "--all-features", "--", "-D", "warnings"])
@@ -32,8 +32,26 @@ def main(cwd: str, cargo: Dict[str, str]):
         for cmd in gitworkflow:
             sp.call(cmd)
     
+def debug(cwd: str, cargo: Dict[str, str]):
+    sp.call(["cargo", "run"] + sys.argv[2:])
+
+def run(cwd: str, cargo: Dict[str, str]):
+    sp.call(["cargo", "run", "--release"] + sys.argv[2:])
+
+def test(cwd: str, cargo: Dict[str, str]):
+    sp.call(["cargo", "test"] + sys.argv[2:])
+
+def fmt(cwd: str, cargo: Dict[str, str]):
+    sp.call(["cargo", "fmt"] + sys.argv[2:])
+
+def lint(cwd: str, cargo: Dict[str, str]):
+    sp.call(["cargo", "clippy"] + sys.argv[2:])
+
+def doc(cwd: str, cargo: Dict[str, str]):
+    sp.call(["cargo", "doc"] + sys.argv[2:])
 
 if __name__ == "__main__":
+
     cwd = os.getcwd()
     if not os.path.exists(f"{cwd}/Cargo.toml"):
         print("Not inside a crate!")
@@ -49,5 +67,18 @@ if __name__ == "__main__":
             elif sublevel is not None:
                 split = list(map(lambda x: x.strip(), l.split("=")))
                 cargo[sublevel][split[0]] = split[1].replace('"', '')
-        main(cwd, cargo)
+        if "commit" == sys.argv[1]:
+            commit(cwd, cargo)
+        elif "debug" == sys.argv[1]:
+            debug(cwd, cargo)
+        elif "run" == sys.argv[1]:
+            run(cwd, cargo)
+        elif "test" == sys.argv[1]:
+            test(cwd, cargo)
+        elif "fmt" == sys.argv[1]:
+            fmt(cwd, cargo)
+        elif "lint" == sys.argv[1]:
+            lint(cwd, cargo)
+        elif "doc" == sys.argv[1]:
+            doc(cwd, cargo)
 
